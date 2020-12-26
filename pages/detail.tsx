db1832fcec9a0c25dfcf6ae6bde98b4c917dd5e1
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-12-26 22:47:38 +0900

    create: Detail page

---
+++ pages/detail.tsx
@@ -0,0 +1,22 @@
+import React, { FC } from 'react';
+import { motion } from 'framer-motion';
+import {
+  pageStyle,
+  pageTransition,
+  pageVariants
+} from '../theme';
+
+const Detail: FC = () => {
+  return (
+    <motion.div
+    initial="initial"
+    animate="in"    
+    exit="out"
+  >
+    <h1>About page</h1>
+  </motion.div>
+
+  )
+}
+
+export default Detail
