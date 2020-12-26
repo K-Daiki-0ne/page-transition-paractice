eacf7bbe22544b26b99acb4697978ddf078c3a0a
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-12-26 22:34:30 +0900

    create: about page transition animation

--- pages/About.tsx
+++ pages/About.tsx
@@ -1,13 +1,23 @@
 import React, { FC } from 'react';
 import { motion } from "framer-motion";
+import {
+  pageStyle,
+  pageTransition,
+  pageVariants
+} from '../theme';
 
 
 const About: FC = () => {
+  const transition = { duration: 1.4, ease: [0.6, 0.01, -0.05, 0.9] };
   return (
     <motion.div
-      initial="initial"
-      animate="in"
-      exit="out"
+      initial={{ opacity: 0, y: 20 }}
+      animate={{
+        opacity: 1,
+        y: 0,
+        transition: { delay: 0.5, ...transition },
+      }}
+      exit="exit"
     >
       <h1>About page</h1>
     </motion.div>
